f8a4d24401ce349c572d18936ba5a54e0ffa071d
Alexis Kaufman <alexiskaufman@gmail.com>
2022-02-12 16:44:58 -0600

    fix

--- functions/routes/groups.js
+++ functions/routes/groups.js
@@ -6,7 +6,7 @@ const db = admin.firestore();
 
 router.post("/create", async (req, res) => {
     const {name} = req.query;
-    let members = ['#FIXME - ADD CALLING USER'];
+    let members = ['//FIXME - ADD CALLING USER'];
     try {
         const docRef = await db.collection("groups").add({
           name, members
@@ -15,11 +15,11 @@ router.post("/create", async (req, res) => {
         res.send({msg: docRef.id});
       } catch (e) {
         console.error("Error adding document: ", e);
-        res.status(400).send({msg: "Error adding document. Check console for error message."});
+        res.status(400).send({message: e.toString()});
       }
 })
 router.post("/addmember", async (req, res) => {
-    const {memberId} = req.query;
+    const {memberId}; //FIXME - ADD CALLING USER
   
 })
 
