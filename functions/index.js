b68f6205002ba2b44ed01e592ab57640b1d7674b
Brad Johnson <2021bradley@gmail.com>
2022-02-12 14:18:37 -0600

    add twilio auth

--- functions/index.js
+++ functions/index.js
@@ -6,18 +6,31 @@
 //   response.send("Hello from Firebase!");
 // });
 
+require("dotenv").config();
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const app = express();
+const twilio = require("twilio");
 
 app.use(cors({ origin: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  return res.status(200).send("Hello World!");
+
+// Authenticate twilio
+const ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+const AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(ACCOUNT_SID, AUTH_TOKEN);
+
+app.get('/send', (req, res) => {
+  client.messages
+    .create({
+      body: "Hi there",
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: process.env.BRADS_PHONE_NUMBER
+    })
+    .then((message) => console.log(message.sid))
+    .done();
 });
 
 exports.app = functions.https.onRequest(app);
