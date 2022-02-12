bc881ac7a53e871be0b498879c4e66d10d7102e9
Brad Johnson <2021bradley@gmail.com>
2022-02-12 13:17:33 -0600

    Init express

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,3 @@
-const functions = require("firebase-functions");
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -7,3 +5,19 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(cors({ origin: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  return res.status(200).send("Hello World!");
+});
+
+exports.app = functions.https.onRequest(app);
