4c6cdb3e37c809e1be09d624ccbb4895304f5b02
realprogrammer5000 <me@maxhogan.dev>
2022-02-12 14:22:07 -0600

    Add android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'tech.friendadvisor',
   appName: 'FriendAdvisor',
   webDir: 'build',
   bundledWebRuntime: false
