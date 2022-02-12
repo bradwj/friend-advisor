005289cbec393c39b10b8a1334a25c873290d228
realprogrammer5000 <me@maxhogan.dev>
2022-02-12 16:05:44 -0600

    Fix

--- functions/index.js
+++ functions/index.js
@@ -4,10 +4,6 @@ require("./firebase");
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const { initializeApp, applicationDefault, cert } = require("firebase-admin/app");
-const { getFirestore, Timestamp, FieldValue } = require("firebase-admin/firestore");
-
-initializeApp();
 
 const app = express();
 
