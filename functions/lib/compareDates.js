941487617b402b6fb452dee6f48a437dc1e72558
Alexis Kaufman <alexiskaufman@gmail.com>
2022-02-12 20:36:50 -0600

    push

--- functions/lib/compareDates.js
+++ functions/lib/compareDates.js
@@ -1,21 +1,22 @@
 const admin = require("../firebase.js");
 const db = admin.firestore();
 
+// Call with hourDiff as a numerical parameter, representing the time between the current OS time and the event/nirthday
 
-export async function searchBirthdays(hourDiff)
+export async function searchDates(hourDiff)
 {
     const groupsRef = db.collection('groups');
     const usersRef = db.collection('users');
 
-    const snapshot = await groupsRef.get();
+    const snapshot = await groupsRef.get(); //Obtain all groups
     if (snapshot.empty) {
         console.log('No matching documents.');
         return;
     }  
 
-    snapshot.forEach(groupDoc => {
+    snapshot.forEach(groupDoc => { //Loop through all groups
         const groupData = groupDoc.data().members;
-        groupData.forEach(async user => {
+        groupData.forEach(async user => { //Loop through all group members
             const currentUser = await usersRef.doc(user).get();
 
             //console.log('User found: '+currentUser.id);
@@ -27,7 +28,7 @@ export async function searchBirthdays(hourDiff)
             //console.log(hoursDifferent, userDate, currentDate);
             if (hoursDifferent <= hourDiff && hoursDifferent >= 0)
             {
-                console.log("It is "+(currentUser.data().name)+"'s birthday in approx. "+ (Math.ceil(hoursDifferent)) + " hours!");
+                console.log("It is "+(currentUser.data().name)+"'s birthday in approx. "+ (Math.ceil(hoursDifferent)) + " hours!"); 
             }
         })
     });
