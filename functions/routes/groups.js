c4666730fc86e15b20221ffb59e0f7e85f295097
Alexis Kaufman <alexiskaufman@gmail.com>
2022-02-14 15:39:44 -0600

    began work on group routing, join codes

--- functions/routes/groups.js
+++ functions/routes/groups.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const admin = require("../firebase.js");
+import {generate} from '../lib/createjoincode';
 const db = admin.firestore();
 
 
@@ -9,7 +10,8 @@ router.post("/create", async (req, res) => {
   const { name, creatorId } = req.query;
   const group = {
     name,
-    members: [creatorId]
+    members: [creatorId]//,
+    //id: generate(6)
   }
   try {
     const docRef = await db.collection("groups").add(group);
@@ -23,5 +25,53 @@ router.post("/create", async (req, res) => {
     res.status(400).send({ message: e.toString() });
   }
 });
+
+//Get a specific group
+router.get("/find", findGroup, async (req, res) => {
+  res.status(200).json(res.group);
+})
+
+router.delete("/delete", findGroup, async(req, res) => {
+  try {
+    const deleteDoc = await res.group.delete();
+    res.status(200).json({message: "Group has been deleted successfully!"});
+  } catch (err)
+  {
+    res.status(500).json({message: err.message});
+  }
+  
+})
+
+router.patch("/edit", findGroup, async(req, res) => {
+  //FIXME
+})
+
+async function findGroup(req, res, next) {
+  let group;
+  const { id } = req.query;
+  if (id == null)
+  {
+    try {
+      group = await db.collection("groups").get();
+    } catch (err) {
+      res.status(500).json({message: "Server error: Cannot list groups"})
+    }
+    res.group = group;
+  }
+  else
+  {
+    try {
+      group = await db.collection("groups").doc(id);
+      const doc = await group.get();
+      if (group == null || !doc.exists) {
+          return res.status(404).json({message: "Cannot find group."}) //status 404 means you cannot find something
+      }
+    } catch (err) {
+        return res.status(500).json({message: err.message})
+    }
+    res.group = group;
+  }
+  next();
+}
 
 module.exports = router;
