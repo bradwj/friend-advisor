a921defc9267da480a66d88210c414a02fbf4f9c
Alexis Kaufman <alexiskaufman@gmail.com>
2022-02-15 17:57:26 -0600

    empty the branch

--- functions/lib/scheduler.js
+++
@@ -1,8 +0,0 @@
-const { checkNotifications } = require("./notifications");
-const functions = require("firebase-functions");
-
-exports.scheduledFunction = functions.pubsub.schedule("every 2 hours").onRun((context) => {
-  console.log("This will be run every 2 hours.");
-  checkNotifications();
-  return null;
-});
