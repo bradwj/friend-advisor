d164583614fe5ef4cb34f04c825402573abd347f
Brad Johnson <2021bradley@gmail.com>
2022-02-16 00:14:07 -0600

    fixed eslint complaining about single quotes

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 require("./firebase");
 
 const functions = require("firebase-functions");
-const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerJsdoc = require("swagger-jsdoc");
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
@@ -11,17 +11,17 @@ const app = express();
 
 const options = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'Routes',
-      version: '1.0.0',
-    },
+      title: "Routes",
+      version: "1.0.0"
+    }
   },
-  apis: ['./routes/events.js','./routes/groups.js','./routes/notifications.js'], 
+  apis: ["./routes/events.js", "./routes/groups.js", "./routes/notifications.js"]
 };
 
 const openapiSpecification = swaggerJsdoc(options);
-fs.writeFileSync("routesApi.json", JSON.stringify(openapiSpecification))
+fs.writeFileSync("routesApi.json", JSON.stringify(openapiSpecification));
 
 app.use(cors());
 
