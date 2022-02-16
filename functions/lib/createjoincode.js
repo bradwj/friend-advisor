4d40f5c23d74c97af827c37def0fa723ab016cb5
Alexis Kaufman <alexiskaufman@gmail.com>
2022-02-15 18:29:42 -0600

    changed get all to querying instead

--- functions/lib/createjoincode.js
+++ functions/lib/createjoincode.js
@@ -2,34 +2,26 @@ const admin = require("../firebase.js");
 const db = admin.firestore();
 
 async function generate () {
-  const existingIds = await getAllExistingIds();
   let idLength = 6;
   let attempts = 1;
   let unusedIdToReturn = null;
 
   while (unusedIdToReturn === null) {
     unusedIdToReturn = createCode(idLength);
-
-    for (const groupDoc of existingIds) {
-      const groupData = groupDoc.data();
-      const joinId = groupData.joinId;
-      console.log(joinId, "compared to", unusedIdToReturn);
-      if (joinId === unusedIdToReturn) {
-        unusedIdToReturn = null;
-        break;
-      }
+    console.log("Querying for: ", unusedIdToReturn);
+    const boolExists = await groupExistsWithId(unusedIdToReturn);
+    if (boolExists) {
+      unusedIdToReturn = null;
     }
-
     attempts = attempts + 1;
-    if (attempts >= 50 && unusedIdToReturn === null) {idLength = idLength + 1; attempts = 1;}
+    if (attempts >= 50 && unusedIdToReturn === null) { idLength = idLength + 1; attempts = 1; }
   }
-  
   return unusedIdToReturn;
 }
 
 const createCode = (length) => {
   let result = "";
-  const characters = "ABCDEFGHJKMNOPQRSTUVWXYZabcdefghjkmnopqrstuvwxyz0123456789";
+  const characters = "ABCDEFGHJKMNOPQRSTUVWXYZabcdefghjkmnopqrstuvwxyz0123456789"; 
   const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() *
@@ -38,12 +30,13 @@ const createCode = (length) => {
   return result;
 };
 
-async function getAllExistingIds()
-{
-  group = await db.collection("groups").get();
-  const fixed = [];
-  group.forEach(elem => fixed.push(elem));
-  return fixed;
+async function groupExistsWithId (idToQuery) {
+  const group = await db.collection("groups").where("joinId", "==", idToQuery).get();
+  if (group.empty) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 exports.generate = generate;
