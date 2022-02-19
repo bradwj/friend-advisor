80f6d5050881f236dbf9b55ca32b1ad667265fbd
realprogrammer5000 <max@maxhogan.dev>
2022-02-19 10:47:50 -0600

    Secure /test (fix #43)

--- functions/routes/notifications.js
+++ functions/routes/notifications.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const { checkNotifications } = require("../lib/notifications");
+const admin = require("../firebase");
+const db = admin.firestore();
 
 router.get("/test", async (req, res) => {
-  await checkNotifications();
-  res.status(200).send({ message: "OK" });
+  const adminsDoc = await db.collection("config").doc("admins").get();
+
+  if (adminsDoc.exists && adminsDoc.data().adminUserIds && adminsDoc.data().adminUserIds.length && adminsDoc.data().adminUserIds.includes(req.user.uid)) {
+    await checkNotifications();
+    res.status(200).send({ message: "OK" });
+  } else {
+    res.status(403).send({ message: "Access Denied" });
+  }
 });
 
 module.exports = router;
