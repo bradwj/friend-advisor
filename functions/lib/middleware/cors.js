e6807327b2c8a1fa2113708d0d26bcdf37895a18
Brad Johnson <2021bradley@gmail.com>
2022-02-18 22:53:38 -0600

    update cors allowed methods

--- functions/lib/middleware/cors.js
+++ functions/lib/middleware/cors.js
@@ -2,6 +2,7 @@ const allowOrigin = (req, res, next) => {
   res.set("Access-Control-Allow-Origin",
     process.env.NODE_ENV === "development" ? "*" : "https://friendadvisor.tech"
   );
+  res.set("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, OPTIONS, PATCH");
   next();
 };
 
